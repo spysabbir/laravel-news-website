a2ca0effc2fd48b5dd9a501423d5bc7bb1e20f0c
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-08 11:59:56 +0600

    Checking frontend and fix some bug

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Session;
 
 class Authenticate extends Middleware
 {
@@ -15,6 +16,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            Session::flash('error', 'You must be logged in to access this page.');
             return route('login');
         }
     }
